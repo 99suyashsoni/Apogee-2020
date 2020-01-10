2a7fb71cd47e849020f0ceb8dd4c7e8e302201bc
ishita Aggarwal <e.agrawal03@gmail.com>
2020-01-10 17:17:00 +0530

    Added basic ui implementation

--- lib/events/data/EventsModel.dart
+++ lib/events/data/EventsModel.dart
@@ -1,5 +1,17 @@
+import 'package:apogee_main/events/data/dataClasses/EventsData.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:apogee_main/events/data/database/EventsDao.dart';
 
 class EventsModel with ChangeNotifier{
+EventsDao _eventsDao;
+List<EventsData> events;
 
+EventsModel(){
+  this._eventsDao = EventsDao();
+
+}
+
+Future<List<EventsData>> getAllEvents() async{
+  return await _eventsDao.getAllEvents();
+}
 }
--- lib/events/eventsScreen.dart
+++ lib/events/eventsScreen.dart
@@ -1,4 +1,7 @@
+import 'dart:js';
+
 import 'package:apogee_main/events/data/EventsModel.dart';
+import 'package:apogee_main/shared/UIMessageListener.dart';
 import 'package:flutter/material.dart';
 import 'package:apogee_main/shared/screen.dart';
 import 'package:provider/provider.dart';
@@ -7,25 +10,61 @@ class EventsScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
 
-    return ChangeNotifierProvider<EventsModel>(
-    create: (BuildContext context) => EventsModel(),
-    child: Screen(
+    return  Screen(
       title: "Events",
-      child: EventList(),
+      child:ChangeNotifierProvider<EventsModel>(
+         create: (BuildContext context) => EventsModel(),
+         child: EventList(),
+         ),
       selectedTabIndex: 0,
-    ), 
     );
   
   }
 
 }
 
-class EventList extends StatelessWidget{
+class EventList extends StatelessWidget implements UIMessageListener{
+
   @override
   Widget build(BuildContext context) {
-    return Container(
+    final _eventsModel = Provider.of<EventsModel>(context);
+
+    return Expanded(
+      child:FutureBuilder(
+        builder: (context,snapshot){
+          if(snapshot.connectionState == ConnectionState.waiting)
+            return CircularProgressIndicator();
+          else if(snapshot.connectionState == ConnectionState.done){
+            //TODO: update list
+          }
+          else {
+            onAlertMessageRecived(message: "Something went wrong!",);
+            return Container();
+          }    
+        },
+      ) ,
 
     );
+  }
+
+  @override
+  void onAlertMessageRecived({String message, String title = "Alert", List<Widget> actions}) {
+    // TODO: implement onAlertMessageRecived
+  }
+
+  @override
+  void onAuthenticationExpiered() {
+    // TODO: implement onAuthenticationExpiered
+  }
+
+  @override
+  void onSnackbarMessageRecived({String message}) {
+    // TODO: implement onSnackbarMessageRecived
+  }
+
+  @override
+  void onToastMessageRecived({String message}) {
+    // TODO: implement onToastMessageRecived
   }
 
 }
