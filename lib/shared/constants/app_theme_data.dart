5b8acaaa6df6487913587108f23e36aa15228f87
Prarabdh Garg <prarabdhgrg@gmail.com>
2020-02-20 18:22:17 +0530

    Complete UI for orders screen

--- lib/shared/constants/app_theme_data.dart
+++ lib/shared/constants/app_theme_data.dart
@@ -16,7 +16,7 @@ final appThemeData = ThemeData(
     splashColor: Colors.blue,
     textTheme: TextTheme(
         button: TextStyle(fontSize: 18, color: Colors.white, fontWeight: FontWeight.bold,),
-        title: TextStyle(fontSize: 24, color: Colors.black, fontWeight: FontWeight.w900,fontFamily: 'Google-Sans',fontStyle: FontStyle.normal),
+        title: TextStyle(fontSize: 34, color: Colors.white, fontWeight: FontWeight.w900, fontFamily: 'Google-Sans', fontStyle: FontStyle.normal),
         subtitle: TextStyle(fontSize: 22, color: Colors.grey),
         body1: TextStyle(fontSize: 20)
     )
